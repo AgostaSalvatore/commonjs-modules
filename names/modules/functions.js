2f6af008cd7712a2e78a7213b4b4b9127e782d07
AgostaSalvatore <agosta.boolean@gmail.com>
2025-04-14 13:25:11 +0200

    Add CommonJS module structure for names

---
+++ names/modules/functions.js
@@ -0,0 +1,11 @@
+//definisco la funzione con i parametri firstName e lastName
+function person(firstName, lastName) {
+    return {
+        firstName,
+        lastName
+    }
+}
+
+//esporto la funzione
+module.exports = person;
+console.log(person);
