0e37d8f607f171e53bc1ea832ac9d98f0951cbe2
AgostaSalvatore <agosta.boolean@gmail.com>
2025-04-14 13:28:40 +0200

    add common js structure for hobbies

---
+++ hobbies/modules/functions.js
@@ -0,0 +1,8 @@
+//inizializzo la funzione per i 3 hobbies
+
+function hobbies(hobbyOne, hobbyTwo, hobbyThree) {
+    return [hobbyOne, hobbyTwo, hobbyThree]
+}
+
+module.exports = hobbies;
+console.log(hobbies);
